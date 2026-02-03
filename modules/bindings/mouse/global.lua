df602c870959b5c9c4f443a6c5016cbd331429ef
SD5th <vlaka@ro.ru>
2026-02-03 05:50:40 +0300

    Removed tag switching with mouse wheel on desktop

--- modules/bindings/mouse/global.lua
+++ modules/bindings/mouse/global.lua
@@ -3,7 +3,6 @@ local gears = require("gears")
 
 
 local globalbuttons = gears.table.join(
-	awful.button({}, 4, awful.tag.viewnext),
-	awful.button({}, 5, awful.tag.viewprev)
+
 )
 return globalbuttons
